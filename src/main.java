7b19594de5f589c9ebb83487046e4fa3246487b4
unknown <cougartech@HS148T1600-05.hflcampus.monroe.edu>
2014-12-13 10:42:23 -0500

    merged all the the stuff

---
+++ src/main.java
@@ -0,0 +1,72 @@
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		int data[][];
+		
+		double x = -1;
+		double y = -1;
+		int tDir = -1;
+		int pDir = 1;
+		double accur = 0.5;
+		
+		int xLoc = 0;
+		int yLoc = 0;
+		
+		while(true){
+			
+			if(y == tDir && x != pDir){
+				
+				x = x + (pDir * accur);
+				xLoc = xLoc + pDir;
+				tDir = tDir * -1;
+		
+			}
+			else if(y == tDir && x == pDir){
+				
+				tDir *= -1;
+				pDir *= -1;
+				y = y + (tDir * accur);
+				if(y == 1.0){
+					yLoc = 0;
+				}
+				if(y == 0.5){
+					yLoc = 1;
+				}
+				if(y == 0){
+					yLoc = 2;
+				}
+				if(y == -0.5){
+					yLoc = 3;
+				}
+				if(y == -1){
+					yLoc = 4;
+				}
+				
+			}else{
+				
+				y = y + (tDir * accur);
+				if(y == 1.0){
+					yLoc = 0;
+				}
+				if(y == 0.5){
+					yLoc = 1;
+				}
+				if(y == 0){
+					yLoc = 2;
+				}
+				if(y == -0.5){
+					yLoc = 3;
+				}
+				if(y == -1){
+					yLoc = 4;
+				}
+				
+			}
+				
+			System.out.println(x + ", " + y + " | " + xLoc + ", " + yLoc);
+			
+		}
+	}		
+}
